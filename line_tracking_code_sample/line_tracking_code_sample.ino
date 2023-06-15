50662ca303d1eee85e2043ad1cf56ce97eb6d412
smyt022 <tocatsa@gmail.com>
2023-06-15 15:09:37 -0400

    added lux sensor

--- line_tracking_code_sample/line_tracking_code_sample.ino
+++ line_tracking_code_sample/line_tracking_code_sample.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_MotorShield.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
-
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_LSM303_U.h>
+#include <hp_BH1750.h>//light sensor
 #define IR_PIN_1 A0
 #define IR_PIN_2 A1
 #define LEFT_MOTOR_PIN_1 1
@@ -19,10 +19,20 @@ int sensorValue1;
 int sensorValue2;
 int MAX_SPEED = 255;
 
+hp_BH1750 lightSensor;//
+
+int lightTurnedOn = 0;//kinda like a boolean
+int LUX_THRESHOLD = 3000;
 
 void setup() {
-  pinMode(IR_PIN, INPUT);
-  AFMS.begin();  //sets up the communication between the microcontroller (Arduino) and the Adafruit Motor Shield (Similar to serial.begin). Not sure if necessary.
+  //IR pins
+  pinMode(IR_PIN_1, INPUT);
+  pinMode(IR_PIN_2, INPUT);
+
+  //driver
+  AFMS.begin(); 
+  
+  //motors
   LEFT_MOTOR_1->setSpeed(0);
   LEFT_MOTOR_2->setSpeed(0);
   RIGHT_MOTOR_3->setSpeed(0);
@@ -31,27 +41,45 @@ void setup() {
   LEFT_MOTOR_2->run(RELEASE);
   RIGHT_MOTOR_3->run(RELEASE);
   RIGHT_MOTOR_4->run(RELEASE);
+
+  //light sensor
+  lightSensor.begin(BH1750_TO_GROUND);
 }
 
 void loop() {
 
-  //black returns 1, white returns 0
-  sensorValue1 = digitalRead(IR_PIN_1); //left sensor
-  sensorValue2 = digitalRead(IR_PIN_2); //right sensor
+  //light sensor (robot initialization step)
+  if(lightTurnedOn == 0){
+    lightSensor.start();
+    int lux = lightSensor.getLux();
 
-  // Directions: (assume the IR sensors are on either side of the black tape)
-  if (sensorValue1==0 && sensorValue2==0) { //on either side of the tape
-    //straight
-    straight();
-  } else if (sensorValue1==1 && sensorValue2==1){//stop, T shape end
-    //stop
-    stop();
-  } else if( sensorValue1==1){ //left turn
-    //left 
-    left();
-  } else if (sensorValue2==1){ //right turn
-    //right
-    right();
+    //determine if light is shining on
+
+    if(lux>3000){
+      lightTurnedOn = 1;
+    }
+
+  }
+
+  if(lightTurnedOn == 1){
+    //black returns 1, white returns 0
+    sensorValue1 = digitalRead(IR_PIN_1); //left sensor
+    sensorValue2 = digitalRead(IR_PIN_2); //right sensor
+
+    // Directions: (assume the IR sensors are on either side of the black tape)
+    if (sensorValue1==0 && sensorValue2==0) { //on either side of the tape
+      //straight
+      straight();
+    } else if (sensorValue1==1 && sensorValue2==1){//stop, T shape end
+      //stop
+      stop();
+    } else if( sensorValue1==1){ //left turn
+      //left 
+      left();
+    } else if (sensorValue2==1){ //right turn
+      //right
+      right();
+    }
   }
 
 }
@@ -59,10 +87,10 @@ void loop() {
 void straight(){
   //Directions:
   LEFT_MOTOR_1->run(BACKWARD);
-  LEFT_MOTOR_2->run(BACKWARD);
+  LEFT_MOTOR_2->run(FORWARD);
     //remember, left and right motors are set up in opposite directions
   RIGHT_MOTOR_3->run(FORWARD);
-  RIGHT_MOTOR_4->run(FORWARD);
+  RIGHT_MOTOR_4->run(BACKWARD);
 
   //Speed:
   LEFT_MOTOR_1->setSpeed(MAX_SPEED);
@@ -74,15 +102,15 @@ void straight(){
 void right(){
   //Directions:
   LEFT_MOTOR_1->run(BACKWARD);
-  LEFT_MOTOR_2->run(BACKWARD);
+  LEFT_MOTOR_2->run(FORWARD);
     //remember, left and right motors are set up in opposite directions
   RIGHT_MOTOR_3->run(FORWARD);
-  RIGHT_MOTOR_4->run(FORWARD);
+  RIGHT_MOTOR_4->run(BACKWARD);
 
   //Speed:
     //note: left goes slow, right stays still
-  LEFT_MOTOR_1->setSpeed(50);//less than 20% of max speed
-  LEFT_MOTOR_2->setSpeed(50);//less than 20% of max speed
+  LEFT_MOTOR_1->setSpeed(150);//less than 20% of max speed
+  LEFT_MOTOR_2->setSpeed(150);//less than 20% of max speed
   RIGHT_MOTOR_3->setSpeed(0);
   RIGHT_MOTOR_4->setSpeed(0);
 }
@@ -90,17 +118,17 @@ void right(){
 void left(){
   //Directions:
   LEFT_MOTOR_1->run(BACKWARD);
-  LEFT_MOTOR_2->run(BACKWARD);
+  LEFT_MOTOR_2->run(FORWARD);
     //remember, left and right motors are set up in opposite directions
   RIGHT_MOTOR_3->run(FORWARD);
-  RIGHT_MOTOR_4->run(FORWARD);
+  RIGHT_MOTOR_4->run(BACKWARD);
 
   //Speed:
     //note: right goes slow, left stays still
   LEFT_MOTOR_1->setSpeed(0);//less than 20% of max speed
   LEFT_MOTOR_2->setSpeed(0);//less than 20% of max speed
-  RIGHT_MOTOR_3->setSpeed(50);
-  RIGHT_MOTOR_4->setSpeed(50);
+  RIGHT_MOTOR_3->setSpeed(150);
+  RIGHT_MOTOR_4->setSpeed(150);
 }
 
 void stop(){
